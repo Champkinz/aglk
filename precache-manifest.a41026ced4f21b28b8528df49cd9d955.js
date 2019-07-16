f89b8ebaa3182e01ed7d3a5ce4f80f5c9558da75
Charana Amarasekara <champ@Charanas-MacBook-Pro.local>
2019-07-16 12:17:58 +0530

    Updates

---
+++ precache-manifest.a41026ced4f21b28b8528df49cd9d955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e7d4cecc0432079fc244efee8c1b76",
+    "url": "/aglk/index.html"
+  },
+  {
+    "revision": "98014d1f7109fca91cf6",
+    "url": "/aglk/static/css/2.7c2c7a7e.chunk.css"
+  },
+  {
+    "revision": "0e8a5be2d479bbc403f6",
+    "url": "/aglk/static/css/main.c7b7a933.chunk.css"
+  },
+  {
+    "revision": "98014d1f7109fca91cf6",
+    "url": "/aglk/static/js/2.ea0ae1bd.chunk.js"
+  },
+  {
+    "revision": "0e8a5be2d479bbc403f6",
+    "url": "/aglk/static/js/main.e399e26c.chunk.js"
+  },
+  {
+    "revision": "f6447f10981feae87f1d",
+    "url": "/aglk/static/js/runtime~main.26dc4227.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aglk/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28f95cb40f5666e9e640dc306ff25399.js"
+  "/aglk/precache-manifest.a41026ced4f21b28b8528df49cd9d955.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aglk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
