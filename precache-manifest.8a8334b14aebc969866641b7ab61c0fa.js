1e65deda3cc66051778f049c8763ed791b8e4615
Charana Amarasekara <champ@Charanas-MacBook-Pro.local>
2019-07-16 10:55:43 +0530

    Updates

---
+++ precache-manifest.8a8334b14aebc969866641b7ab61c0fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e46e0c22d0943ed4a2659ae84a7abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7b619b6836191bc621",
+    "url": "/static/css/2.7c2c7a7e.chunk.css"
+  },
+  {
+    "revision": "68db4a2764bc327807cf",
+    "url": "/static/css/main.c7b7a933.chunk.css"
+  },
+  {
+    "revision": "fa7b619b6836191bc621",
+    "url": "/static/js/2.d565b24b.chunk.js"
+  },
+  {
+    "revision": "68db4a2764bc327807cf",
+    "url": "/static/js/main.a243978b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "166.62.6.39/precache-manifest.0a62cd98dff8f8a44e26fb53b0463901.js"
+  "/precache-manifest.8a8334b14aebc969866641b7ab61c0fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("166.62.6.39/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
